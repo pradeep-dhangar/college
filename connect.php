91871aaa44ae937e365f125fb17ac5554c62c0b4
Pradeep <pradeepdhangarkabaddi@gmail.com>
2023-02-08 08:07:46 +0530

    first

---
+++ connect.php
@@ -0,0 +1,23 @@
+<?php
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$gender = $_POST['gender'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$number = $_POST['number'];
+
+$conn = new mysqli('localhost', 'root', '', 'test');
+if($conn->connect_error){
+die('Connection failed :' .$conn->connect_error);
+}
+else{
+   $stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, number)
+   values(?, ?, ?, ?, ?, ?)");
+   $stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
+   $stmt->execute();
+   echo "registration succesfully";
+   $stmt->close();
+   $conn->close();
+}
+
+?>
